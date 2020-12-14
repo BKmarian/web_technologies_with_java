ae5cbb0c23208d27866feb2b656277608e96970a
mariachitu <32489883+mariachitu@users.noreply.github.com>
2020-12-14 22:29:35 +0200

    Laborator 10

---
+++ lab10/src/main/java/com/example/lab10/controller/OrderController.java
@@ -0,0 +1,42 @@
+package com.example.lab10.controller;
+
+import com.example.lab10.exception.NoProductFoundException;
+import com.example.lab10.exception.NoStockAvailableException;
+import com.example.lab10.model.OrderItem;
+import com.example.lab10.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity create(@RequestBody List<OrderItem> items) {
+        try {
+            return ResponseEntity.ok(orderService.save(items));
+        }   catch (NoProductFoundException | NoStockAvailableException e)   {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/cancel")
+    public void cancel()    {
+//        TODO: Tema 3: Implementati un endpoint responsabil cu anularea unei comanzi plasate:
+//              - schimbati statusul comenzii
+//              - actualizati stocul produselor corespunzator
+//              - deadline:  6 ianuarie 2021
+
+    }
+}
---
+++ lab10/src/main/java/com/example/lab10/repository/OrderRepository.java
@@ -0,0 +1,35 @@
+package com.example.lab10.repository;
+
+import com.example.lab10.model.Order;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+
+@Repository
+public class OrderRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public OrderRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public long save(Order order)   {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        String insertSql = "INSERT INTO orders VALUES(NULL, ?, ?)";
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(insertSql, new String[] {"id"});
+            ps.setDouble(1, order.getTotalPrice());
+            ps.setString(2, order.getStatus().toString());
+            return ps;
+        }, keyHolder);
+
+        return (long) keyHolder.getKey().longValue();
+
+        //jdbcTemplate.update(insertSql, order.getTotalPrice(), order.getStatus().toString());
+    }
+}
